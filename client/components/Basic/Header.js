b90784c3f0c3172268e6bebffe34a98755bfc60e
maxmomo <32480503+maxmomo@users.noreply.github.com>
2023-10-22 16:59:20 +0200

    [REF] Header

--- client/components/Basic/Header.js
+++ client/components/Basic/Header.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { useMyContext } from '../../context/MyContext';
+
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import colors from '../../constants/colors';
 
 function Header({ navigation }) {
 
-    const { state, dispatch } = useMyContext();
-
     if (navigation) {
         return (
             <View style={styles.header}>
                 <TouchableOpacity onPress={() => navigation.goBack()} style={styles.icon}>
-                    <MaterialCommunityIcons name='arrow-left' size={30} color='#F2C238' />
+                    <MaterialCommunityIcons name='arrow-left' size={30} color={colors.theme} />
                 </TouchableOpacity>
                 <View style={styles.title}>
                     <Image source={require('../../assets/header.png')} style={styles.logo} />
                 </View>
                 <TouchableOpacity style={styles.icon}>
-                    <MaterialCommunityIcons name='account-circle' size={30} color='#F2C238' />
+                    <MaterialCommunityIcons name='account-circle' size={30} color={colors.theme} />
                 </TouchableOpacity>
             </View>
         );
@@ -26,13 +24,13 @@ function Header({ navigation }) {
         return (
             <View style={styles.header}>
                 <TouchableOpacity style={styles.icon}>
-                    <MaterialCommunityIcons name='arrow-left' size={30} color='#21222D' />
+                    <MaterialCommunityIcons name='arrow-left' size={30} color={colors.backgroundLight} />
                 </TouchableOpacity>
                 <View style={styles.title}>
                     <Image source={require('../../assets/header.png')} style={styles.logo} />
                 </View>
                 <TouchableOpacity style={styles.icon}>
-                    <MaterialCommunityIcons name='account-circle' size={30} color='#F2C238' />
+                    <MaterialCommunityIcons name='account-circle' size={30} color={colors.theme} />
                 </TouchableOpacity>
             </View>
         );
@@ -46,7 +44,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row'
     },
     icon: {
-        color: '#F2C238',
+        color: colors.theme,
         flex: 1,
         alignItems: 'center'
     },
